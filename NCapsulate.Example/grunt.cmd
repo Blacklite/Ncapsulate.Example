0552a980e53d0fd00ec2bb5149ad929856eef23a
David Driscoll <david.driscoll@gmail.com>
2014-04-17 09:44:08 -0400

    Added Grunt, and example grunt file.
    This is a simplified example, but demonstrates that Grunt also works.

---
+++ NCapsulate.Example/grunt.cmd
@@ -0,0 +1,3 @@
+@echo off
+\nodejs\node ..\packages\Ncapsulate.Grunt.0.4.4\nodejs\node_modules\grunt-cli\bin\grunt --no-color %*
+@echo on
--- NCapsulate.Example/gulp.cmd
+++ NCapsulate.Example/gulp.cmd
@@ -1,3 +1,3 @@
 @echo off
-..\packages\Ncapsulate.Node.0.10.26\nodejs\node ..\packages\Ncapsulate.Gulp.3.6.0\nodejs\node_modules\gulp\bin\gulp --no-color %*
+\nodejs\node ..\packages\Ncapsulate.Gulp.3.6.1\nodejs\node_modules\gulp\bin\gulp --no-color %*
 @echo on
